da42f9a96e25d107531529eb6e9d2099c3707ad4
Nadia Mughal <nmughal14@gmail.com>
2017-04-02 21:58:06 -0400

    Added a docblock to inventory service function

--- src/js/inventory.service.js
+++ src/js/inventory.service.js
@@ -4,25 +4,45 @@
   angular.module('inventory')
     .factory('InventoryService', InventoryService);
 
+    /**
+     * Creates a new Inventory Service
+     */
     function InventoryService(){
-      console.log('Creating a new InventoryService');
 
       let items = JSON.parse(localStorage.getItem('items')) || [];
 
 
       /**
-      * Adds a name to the list
+      * Adds an item to the inventory
       * @param  {String} name The item to add with a name, price, discount, color, and quantity
       * @return {void}
       */
       function addItem(item) {
 
-        if(typeof(item) !== 'object' || typeof(item.name) !== 'string' || item.name.length < 1) {
+        if(typeof(item) !== 'object') {
+          return;
+        }
+
+        if(typeof(item.name) !== 'string' || item.name.length < 1) {
+          return;
+        }
+
+        if(typeof(item.color) !== 'string'){
           return;
         }
 
         item.price = Number(item.price);
         if(Number.isNaN(item.price) || item.price < 0.01) {
+          return;
+        }
+
+        item.discount = Number(item.discount);
+        if(Number.isNaN(item.discount) || item.discount < 0.01) {
+          return;
+        }
+
+        item.quantity = Number(item.quantity);
+        if(Number.isNaN(item.quantity) || item.quantity < 0) {
           return;
         }
 
@@ -40,13 +60,12 @@
       }
 
       /**
-       * Gets the things in the array
-       * @return {void}
+       * Gets the items in the array
+       * @return {Array}
        */
       function getAllItems() {
         return items;
       }
-
 
       return {
         addItem: addItem,
